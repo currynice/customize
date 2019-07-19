85b1c1149abbb5e54b15bcef611133efb2fe1f26
chengxinyu <694975984@qq.com>
2019-07-19 18:00:09 +0800

    719

--- src/main/java/com/cxy/customize/core/text/ASCIIStrCache.java
+++ src/main/java/com/cxy/customize/core/text/ASCIIStrCache.java
@@ -2,6 +2,7 @@ package com.cxy.customize.core.text;
 
 
 /**
+ * ASCII一共128个 包括空格、标点符号、数字、大小写字母和一些不可见字符。由于总共才128个字符，所以可以使用1个字节来进行编码
  * 维护了一个ASCII字符对应的字符串缓存
  */
 public class ASCIIStrCache {
@@ -16,7 +17,7 @@ public class ASCIIStrCache {
 
     /**
      * 字符转为字符串<br>
-     * 如果为ASCII字符，从缓存中取，避免不必要的String对象
+     * 如果为ASCII字符，从缓存中取，避免创建不必要的String对象
      * @param c 字符
      * @return 字符串
      */
