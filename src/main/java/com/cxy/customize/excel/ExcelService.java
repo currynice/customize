802dc7458b015f289180c14f7094a71d599b6d7d
cxy <694975984@qq.com>
2020-03-04 09:33:35 +0800

    excel

--- src/main/java/com/cxy/customize/excel/ExcelService.java
+++ src/main/java/com/cxy/customize/excel/ExcelService.java
@@ -189,4 +189,26 @@
 //        }
 //
 //
+//    /**
+//     * 设置某些列的值只能输入预制的数据,显示下拉框.
+//     * @param  sheet  模板sheet页（需要设置下拉框的sheet）
+//     * @param  textlist 下拉框显示的内容
+//     * @param  firstRow  添加下拉框对应开始行
+//     * @param  endRow    添加下拉框对应结束行
+//     * @param  firstCol  添加下拉框对应开始列
+//     * @param  endCol    添加下拉框对应结束列
+//     * @return HSSFSheet 设置好的sheet.
+//     */
+//    public static HSSFSheet setHSSFValidation(HSSFSheet sheet, String[] textlist, int firstRow, int endRow, int firstCol, int endCol){
+//        // 设置数据有效性加载在哪个单元格上,四个参数分别是：起始行、终止行、起始列、终止列
+//        CellRangeAddressList regions = new CellRangeAddressList(firstRow,endRow, firstCol, endCol);
+//        // 加载下拉列表内容
+//        DVConstraint constraint = DVConstraint.createExplicitListConstraint(textlist);
+//        // 数据有效性对象
+//        HSSFDataValidation data_validation_list = new HSSFDataValidation(regions, constraint);
+//        sheet.addValidationData(data_validation_list);
+//        return sheet;
+//    }
+//
+//
 //}
