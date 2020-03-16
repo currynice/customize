dc9c2d0e1f72ccbac716774abc68364ca7598f8f
cxy <694975984@qq.com>
2020-03-16 17:35:45 +0800

    validDate

--- src/main/java/com/cxy/customize/core/util/DateUtil.java
+++ src/main/java/com/cxy/customize/core/util/DateUtil.java
@@ -5,9 +5,12 @@ import com.cxy.customize.core.date.DateField;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 /**
  *
@@ -218,7 +221,43 @@ public class DateUtil {
     }
 
 
+    /**
+     * isValid - dd/MM/yyyy with 20130925 = false
+     * isValid - dd/MM/yyyy with 25/09/2013 = true
+     * isValid - dd/MM/yyyy with 25/09/2013 12:13:50 = false
+     * isValid - yyyy-MM-dd with 2017-18--15 = false
+     * @param format
+     * @param value
+     * @param locale
+     * @return
+     */
+    public static boolean isValidFormat(String format, String value, Locale locale) {
+        LocalDateTime ldt = null;
+        DateTimeFormatter fomatter = DateTimeFormatter.ofPattern(format, locale);
 
+        try {
+            ldt = LocalDateTime.parse(value, fomatter);
+            String result = ldt.format(fomatter);
+            return result.equals(value);
+        } catch (DateTimeParseException e) {
+            try {
+                LocalDate ld = LocalDate.parse(value, fomatter);
+                String result = ld.format(fomatter);
+                return result.equals(value);
+            } catch (DateTimeParseException exp) {
+                try {
+                    LocalTime lt = LocalTime.parse(value, fomatter);
+                    String result = lt.format(fomatter);
+                    return result.equals(value);
+                } catch (DateTimeParseException e2) {
+                    // Debugging purposes
+                    //e2.printStackTrace();
+                }
+            }
+        }
+
+        return false;
+    }
 
 
 
